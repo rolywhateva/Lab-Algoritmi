b6ccedc60ba3fb61626641ce150204b2c1bb121c
veres roland <rolandveres@yahoo.com>
2019-05-20 11:21:01 +0300

    20.05.2019 Colorarea hartii + Poloneza

---
+++ Lab11/Poloneza/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Poloneza
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Dati expresia:");
+            string expresie =@""+ Console.ReadLine();
+            string[] tokens = expresie.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+          
+            Stiva stack = new Stiva();
+            List<string> operators = new List<string>(new string[] { "+", "-", "*", "/" });
+            Console.WriteLine(string.Join(" ", operators.ToArray()));
+            for (int i=0;i<tokens.Length;i++)
+            {
+                if(!operators.Contains(tokens[i]))
+                {
+                    bool success = int.TryParse(tokens[i], out int toAdd);
+                    if(success==false)
+                    {
+                        Console.WriteLine(tokens[i]);
+                        Console.WriteLine("Ilegal1");
+                        return;
+                    }
+                    stack.Add(tokens[i]);
+                }
+                else
+                {
+                    int op1 = int.Parse(stack.Pop());
+                    int op2 = int.Parse(stack.Pop());
+                    int? rez = Rezultat(op2, op1, tokens[i]);
+                    if(rez==null)
+                    {
+                        Console.WriteLine(tokens[i]);
+                        Console.WriteLine("Ilegal2");
+                        return;
+                    }
+                    stack.Add(rez.ToString());
+                }
+                    
+
+            }
+            stack.View();
+
+        }
+
+        private static int? Rezultat(int op1, int op2, string v)
+        {
+            switch(v)
+            {
+                case "+": return op1 + op2 ;
+                case "-":return op1 - op2;
+                case "*":return op1 * op2;
+                case "/":return op1 / op2;
+                default: return null;
+            }
+        }
+    }
+}
---
+++ Lab11/Poloneza/Stiva.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poloneza
+{
+    class Stiva
+    {
+        string[] values;
+        public int n;
+        public Stiva()
+        {
+            n = 0;
+            values = new string[n];
+        }
+        public void Add(string value)
+        {
+            n++;
+            string[] temp = new string[n];
+            for (int i = 0; i <n-1; i++)
+                temp[i] = values[i];
+            temp[n-1] = value;
+            values = temp;
+        }
+        public string Pop()
+        {
+            if (n < 1)
+                return null;
+            string toReturn = values[n-1];
+            string[] temp = new string[--n];
+            for (int i = 0; i < temp.Length; i++)
+                temp[i] = values[i];
+            values = temp;
+            return toReturn;
+        }
+        public void View()
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(values[i] + "\t");
+
+        }
+    }
+}
